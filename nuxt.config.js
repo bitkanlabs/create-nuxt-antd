9777f7a64b70607c6efc571cafe4a60b3b3d320e
yangqiqi <scarlett@bitkan.comscarlett@bitkan.comscarlett@bitkan.comscarlett@bitkan.com>
2018-06-06 16:41:10 +0800

    nuxt project using antd

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+  srcDir: 'src/', // 设置 NUxt js 应用的源码目录
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FFFFFF' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        // config.module.rules.push({
+        //   enforce: 'pre',
+        //   test: /\.(less)$/,
+        //   loader: 'style-loader!css-loadder!less-loader',
+        //   exclude: /(node_modules)/
+        // })
+      }
+    }
+  }
+}
---
+++ src/pages/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    This is home page!! welcome~
+    <button @click="$store.commit('increment')">{{$store.state.counter}}</button>
+    <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
+  </div>
+</template>
+
+
+<script>
+import Vue from 'vue'
+import {Button} from 'vue-antd-ui' // 模块的style属性可以做到按需自动加载
+import '~/static/style/variable.less'
+Vue.component(Button.name, Button)
+  export default {
+    name: '',
+  }
+</script>
+
+<style scoped>
+
+</style>
